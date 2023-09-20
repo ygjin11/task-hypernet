b74fc5161c0a06c046dd45140d3eb1499e2e4d28
yg_Jin <68002027+ygjin11@users.noreply.github.com>
2023-09-20 11:31:09 +0800

    fix bug of model

--- model.py
+++ model.py
@@ -12,9 +12,11 @@ import torch.nn.functional as F
 from torch.distributions.categorical import Categorical
 import torch_ac
 from hypernetwork import HyperNetwork
+
+# Set the device to CUDA device with index 1 (if available)
 device = torch.device("cuda:1")
 
-
+# Function to initialize model parameters
 def init_params(m):
     classname = m.__class__.__name__
     if classname.find("Linear") != -1:
@@ -23,7 +25,7 @@ def init_params(m):
         if m.bias is not None:
             m.bias.data.fill_(0)
 
-
+# Define Actor-Critic model class
 class ACModel(nn.Module, torch_ac.RecurrentACModel):
     def __init__(self, obs_space, action_space, use_memory=False, use_text=False):
         super().__init__()
@@ -62,9 +64,7 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
             nn.Linear(64, 1)
         )
 
-        # self.num = 50
-        # self.hope = HyperNetwork(z_dim=512+self.num)
-
+        # Create an instance of HyperNetwork
         self.hope = HyperNetwork(z_dim=8)
 
         # Initialize parameters correctly
@@ -72,7 +72,7 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
 
     @property
     def memory_size(self):
-        return 2*self.semi_memory_size
+        return 2 * self.semi_memory_size
 
     @property
     def semi_memory_size(self):
@@ -85,66 +85,34 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
         embedding = x
         text = obs.text
 
-        otask1_1 = torch.nn.Parameter(torch.tensor([1, 0, 0, 0, 0, 0, 0, 0]).float().to(device), requires_grad=True)
-        otask1_2 = torch.nn.Parameter(torch.tensor([0, 1, 0, 0, 0, 0, 0, 0]).float().to(device), requires_grad=True)
-        otask1_3 = torch.nn.Parameter(torch.tensor([0, 0, 1, 0, 0, 0, 0, 0]).float().to(device), requires_grad=True)
-        otask1_4 = torch.nn.Parameter(torch.tensor([0, 0, 0, 1, 0, 0, 0, 0]).float().to(device), requires_grad=True)
-        otask1_5 = torch.nn.Parameter(torch.tensor([0, 0, 0, 0, 1, 0, 0, 0]).float().to(device), requires_grad=True)
-        otask1_6 = torch.nn.Parameter(torch.tensor([0, 0, 0, 0, 0, 1, 0, 0]).float().to(device), requires_grad=True)
-        otask1_7 = torch.nn.Parameter(torch.tensor([0, 0, 0, 0, 0, 0, 1 ,0]).float().to(device), requires_grad=True)
+        # Initialize tensors for otask1 and otask2
+        otask1 = torch.eye(8, dtype=torch.float, device=device).requires_grad_()
+        otask2 = torch.cat((torch.eye(7, dtype=torch.float, device=device), torch.ones(7, dtype=torch.float, device=device).unsqueeze(1)), dim=1).requires_grad_()
 
-        otask2_1 = torch.nn.Parameter(torch.tensor([1, 0, 0, 0, 0, 0, 0, 1]).float().to(device), requires_grad=True)
-        otask2_2 = torch.nn.Parameter(torch.tensor([0, 1, 0, 0, 0, 0, 0, 1]).float().to(device), requires_grad=True)
-        otask2_3 = torch.nn.Parameter(torch.tensor([0, 0, 1, 0, 0, 0, 0, 1]).float().to(device), requires_grad=True)
-        otask2_4 = torch.nn.Parameter(torch.tensor([0, 0, 0, 1, 0, 0, 0, 1]).float().to(device), requires_grad=True)
-        otask2_5 = torch.nn.Parameter(torch.tensor([0, 0, 0, 0, 1, 0, 0, 1]).float().to(device), requires_grad=True)
-        otask2_6 = torch.nn.Parameter(torch.tensor([0, 0, 0, 0, 0, 1, 0, 1]).float().to(device), requires_grad=True)
-        otask2_7 = torch.nn.Parameter(torch.tensor([0, 0, 0, 0, 0, 0, 1, 1]).float().to(device), requires_grad=True)
+        # Load the 'task_lang.pt' file
+        task = torch.load('task_lang.pt', map_location=device).float()
 
-
-        task = torch.load('task_lang.pt')
-
-        task1_1 = task[0].float().to(device)
-        task1_2 = task[1].float().to(device)
-        task1_3 = task[2].float().to(device)
-        task1_4 = task[3].float().to(device)
-        task1_5 = task[4].float().to(device)
-        task1_6 = task[5].float().to(device)
-        task1_7 = task[6].float().to(device)
-
-        task2_1 = task[0].float().to(device)
-        task2_2 = task[1].float().to(device)
-        task2_3 = task[2].float().to(device)
-        task2_4 = task[3].float().to(device)
-        task2_5 = task[4].float().to(device)
-        task2_6 = task[5].float().to(device)
-        task2_7 = task[6].float().to(device)
-
+        # Initialize tensors for task1 and task2
+        task1 = task[:7].to(device)
+        task2 = torch.cat((task[:7], torch.ones(7).to(device).unsqueeze(1)), dim=1)
 
         embedding_new = []
+        task_mapping = {
+            1: (task1_1, otask1_1),
+            24: (task1_2, otask1_2),
+            5: (task1_3, otask1_3),
+            9: (task1_4, otask1_4),
+            17: (task1_5, otask1_5),
+            15: (task1_6, otask1_6),
+            26: (task1_7, otask1_7)
+        }
+        task = None
+        otask = None
         for i in range(len(embedding)):
             text[i] = text[i].reshape(-1)
-            if text[i][0].item() == 1:
-                task = task1_1
-                otask = otask1_1
-            if text[i][0].item() == 24:
-                task = task1_2
-                otask = otask1_2
-            if text[i][0].item() == 5:
-                task = task1_3
-                otask = otask1_3
-            if text[i][0].item() == 9:
-                task = task1_4
-                otask = otask1_4
-            if text[i][0].item() == 17:
-                task = task1_5
-                otask = otask1_5
-            if text[i][0].item() == 15:
-                task = task1_6
-                otask = otask1_6
-            if text[i][0].item() == 26:
-                task = task1_7
-                otask = otask1_7
+            item_value = text[i][0].item()
+            if item_value in task_mapping:
+                task, otask = task_mapping[item_value]
 
             matrix, bias = self.hope(task, otask)
             matrix = matrix.reshape(64, 64)
@@ -157,7 +125,7 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
         x = self.critic(embedding_hyper)
         value = x.squeeze(1)
 
-        sample_num = 15
+        sample_num = 50
         sample = random.sample(range(0, 50), sample_num)
         exp_memory_state = [exp_memory_state[i].to(device) for i in sample]
         exp_memory_value = [exp_memory_value[i].reshape(1).to(device) for i in sample]
@@ -166,28 +134,22 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
         exp_memory_state = self.image_conv(exp_memory_state).reshape(exp_memory_state.shape[0], -1)
 
         query = []
+        task_mapping = {
+            1: (task2_1, otask2_1),
+            24: (task2_2, otask2_2),
+            5: (task2_3, otask2_3),
+            9: (task2_4, otask2_4),
+            17: (task2_5, otask2_5),
+            15: (task2_6, otask2_6),
+            26: (task2_7, otask2_7)
+        }
+        task = None
+        otask = None
         for i in range(len(embedding)):
-            if text[i][0].item() == 1:
-                task = task2_1
-                otask = otask2_1
-            if text[i][0].item() == 24:
-                task = task2_2
-                otask = otask2_2
-            if text[i][0].item() == 5:
-                task = task2_3
-                otask = otask2_3
-            if text[i][0].item() == 9:
-                task = task2_4
-                otask = otask2_4
-            if text[i][0].item() == 17:
-                task = task2_5
-                otask = otask2_5
-            if text[i][0].item() == 15:
-                task = task2_6
-                otask = otask2_6
-            if text[i][0].item() == 26:
-                task = task2_7
-                otask = otask2_7
+            item = text[i][0].item()
+            if item in task_mapping:
+                task, otask = task_mapping[item]
+
             matrix, bias = self.hope(task, otask)
             matrix = matrix.reshape(64, 64)
             bias = bias.reshape(64)
@@ -196,29 +158,23 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
         query = torch.cat(query, dim=0)
 
         key = []
+        task_mapping = {
+            1: (task2_1, otask2_1),
+            24: (task2_2, otask2_2),
+            5: (task2_3, otask2_3),
+            9: (task2_4, otask2_4),
+            17: (task2_5, otask2_5),
+            15: (task2_6, otask2_6),
+            26: (task2_7, otask2_7)
+        }
+        task = None
+        otask = None
         for i in range(sample_num):
             exp_memory_text[i] = exp_memory_text[i].reshape(-1)
-            if exp_memory_text[i][0].item() == 1:
-                task = task2_1
-                otask = otask2_1
-            if exp_memory_text[i][0].item() == 24:
-                task = task2_2
-                otask = otask2_2
-            if exp_memory_text[i][0].item() == 5:
-                task = task2_3
-                otask = otask2_3
-            if exp_memory_text[i][0].item() == 9:
-                task = task2_4
-                otask = otask2_4
-            if exp_memory_text[i][0].item() == 17:
-                task = task2_5
-                otask = otask2_5
-            if exp_memory_text[i][0].item() == 15:
-                task = task2_6
-                otask = otask2_6
-            if exp_memory_text[i][0].item() == 26:
-                task = task2_7
-                otask = otask2_7
+            item = exp_memory_text[i][0].item()
+            if item in task_mapping:
+                task, otask = task_mapping[item]
+
             matrix, bias = self.hope(task, otask)
             matrix = matrix.reshape(64, 64)
             bias = bias.reshape(64)
@@ -226,9 +182,7 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
             key.append(key_i)
         key = torch.cat(key, dim=0)
 
-        # query * key
-        # attention_weights.shape(16,100)
-        # attention_weights = torch.mm(embedding_hyper, key.T)
+        # Calculate attention weights
         attention_weights = torch.mm(query, key.T)
         attention_weights = F.softmax(attention_weights, dim=1)
 
@@ -246,5 +200,3 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
     def _get_embed_text(self, text):
         _, hidden = self.text_rnn(self.word_embedding(text))
         return hidden[-1]
-
-
